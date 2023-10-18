5dc2200f43b71a4ca4497f4f0ed55f62d4a6f208
danieldorazio <danieldorazio.97@gmail.com>
2023-10-18 15:47:39 +0200

    add: js logic

---
+++ README.md
@@ -0,0 +1,26 @@
+### Esercizio 
+**Calcolo del prezzo del biglietto del treno**
+
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- Il prezzo del biglietto è definito in base ai km (0. 21 € al km)
+- Va applicato uno sconto del 20% per i minorenni
+- Va applicato uno sconto del 40% per gli over 65.
+
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
+
+### Soluzione 
+
+**Raccolta dati**
+- Chiedere numero di chilomentri da percorrere 
+- Chiedere l'età del passeggero 
+
+**Logica del programma**
+- Calcolare prezzo intero del biglietto
+- Valutare età passegero 
+- Applicare sconto dove è presente 
+- Preparare il messaggio di risultato
+
+**Output**
+Stampa il risultato in HTML
+
--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
-alert("ciao");
+// COMMON
+const prezzoBase = 0.21;
+const scontoMinori = 0.20;
+const scontoOver = 0.40;
+
+
+// RACCOLTA DATI
+const userNome = prompt("Ciao come ti chiami?");
+const userChilometri = parseInt(prompt("Dimmi quanti chilometri vuoi fare"));
+const userEtà = parseInt(prompt("Dimmi quanti anni hai"));
+console.log(userChilometri, userEtà);
+
+
+// LOGICA DEL PROGRAMMA
+const biglettoIntero = (userChilometri * prezzoBase) .toFixed(2) ;
+console.log(biglettoIntero);
+let message = ""
+
+if (userEtà >= 65) {
+    let biglietto  = biglettoIntero - (biglettoIntero * scontoOver);
+    biglietto = biglietto.toFixed(2);
+
+    message = 
+    `
+    <h2> Ciao ${userNome}!</h2>
+    <p> Il costo intero del biglietto per una tratta di ${userChilometri} km e di ${biglettoIntero} € ma visto che te hai ${userEtà} anni hai diritto ad uno sconto del 40% </p>
+    <p> Quindi il prezzo del biglietto con sconto applicato è di ${biglietto} €
+    `
+
+} else if (userEtà <= 17) {
+    let biglietto = biglettoIntero - (biglettoIntero * scontoMinori);
+    biglietto = biglietto.toFixed(2);
+
+    message = 
+    `
+    <h2> Ciao ${userNome}!</h2>
+    <p> Il costo intero del biglietto per una tratta di ${userChilometri} km e di ${biglettoIntero} € ma visto che te hai ${userEtà} anni hai diritto ad uno sconto del 20% </p>
+    <p> Quindi il prezzo del biglietto con sconto applicato è di ${biglietto} €
+    `
+
+} else {
+    let biglietto = biglettoIntero
+    biglietto = biglietto.toFixed(2);
+
+    message = 
+    `
+    <h2> Ciao ${userNome}!</h2>
+    <p> Il costo intero del biglietto per una tratta di ${userChilometri} km e di ${biglettoIntero} € </p>
+    `
+}
+
+// STAMPA IL RISULTATO
+
+document.getElementById("biglietto").innerHTML = message;
+
+
+
